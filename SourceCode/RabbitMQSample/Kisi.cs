6b360e2e44a893daf5c1d8a5d6315fd9362378e3
Serdar GÜL <serdargul@outlook.com>
2023-02-23 15:06:03 +0300

    Comment Commit
    Detaylı açıklama satırları eklendi, türkçeleştirme çalışmaları yapıldı. Kodda bazı güncellemeler gerçekleştirildi

---
+++ SourceCode/RabbitMQSample/Kisi.cs
@@ -0,0 +1,29 @@
+﻿namespace RabbitMQSample
+{
+    /// <summary>
+    /// RabbitMQ teknolojisi ile gönderilecek ve alınacak verilerin içerisinde bulunduğu sınıftır
+    /// </summary>
+    public class Kisi
+    {
+        /// <summary>
+        /// Kişinin ID Bilgisi (Veri Tabanından geliyor gibi düşünebiliriz)
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// Kişinin Adı Bilgisi
+        /// </summary>
+        public string Adi { get; set; }
+        /// <summary>
+        /// Kişinin Soyadı Bilgisi
+        /// </summary>
+        public string Soyadi { get; set; }
+        /// <summary>
+        /// Kişinin Doğum Tarihi Bilgisi
+        /// </summary>
+        public DateTime DogumTarihi { get; set; }
+        /// <summary>
+        /// Kişinin Doğum Yeri Bilgisi
+        /// </summary>
+        public string DogumYeri { get; set; }
+    }
+}
--- SourceCode/RabbitMQSample/Program.cs
+++ SourceCode/RabbitMQSample/Program.cs
@@ -1,21 +1,32 @@
-﻿using Newtonsoft.Json;
+﻿using Bogus;
+using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RabbitMQSample;
 using System.Text;
-using Bogus;
-using RabbitMQ.Client.Events;
 
+/// <summary>
+/// Rabbit MQ uygulamasının konsolda çalışmasını sağlayan metotları içeren temel sınıftır.
+/// </summary>
 public class Program
 {
+    /// <summary>
+    /// Konsol uygulamasının çalıştığı ana metottur
+    /// </summary>
+    /// <param name="args">Gönderilen Argüman Dizizi</param>
     public static void Main(string[] args)
     {
         var programKapansinMi = false;
+        // Kullanıcı Programdan çıkmak için e veya E yazmadığı sürece çalışmasını sağlayan döngüdür
         while (!programKapansinMi) {
             Console.WriteLine("RabbitMQ kuyruğuna kaç tane kayıt göndermek istiyorsunuz? (Programdan çıkmak için E veya e yazınız)");
             var sayi = Console.ReadLine();
-            if (int.TryParse(sayi, out int sayac))
+            // eğer geçerli bir sayı girerse o sayı kadar Kişi kaydı oluşturup
+            // RabbitMQ kuyruğuna yollayacak, alacak
+            // Ve bu bilgileri konsol ekranında yazdıracak kod bloğu burasıdır
+            if (int.TryParse(sayi, out int kisiSayisi))
             {
-                RabbitMQKuyrugaGonder(sayac);
+                RabbitMQKuyrugaGonder(kisiSayisi);
                 Console.WriteLine("------------------------------------------");
                 Console.WriteLine("RabbitMQ kuyruğundan alma işlemleri için ekrana bir değer giriniz");
                 Console.ReadLine();
@@ -24,8 +35,11 @@ public class Program
             }
             else
             {
+                // Eğer e veya E harfi girerse kullanıcı konsoldan
+                // programdan çıkış yapılmasını sağlayacak kod bloğudur
                 if (sayi == "e" || sayi == "E")
                     programKapansinMi = true;
+                // eğer geçersiz bir sayı veya metin girerse gösterilecek hata mesajıdır
                 else
                     Console.WriteLine($"{sayi} geçerli bir sayı veya uygulama kapatma isteği değildir");
             }
@@ -34,58 +48,81 @@ public class Program
         Environment.Exit(0);
     }
 
-    private static void RabbitMQKuyrugaGonder(int sayac)
+    /// <summary>
+    /// Ekrandan alınan sayı kadar Kişi oluşturup
+    /// Rabbit MQ kuyruğuna göndermeyi ve gönderilen kişiler ile ilgili bilgileri
+    /// Konsol ekranında göstermeye yarayan metottur
+    /// </summary>
+    /// <param name="kisiSayisi">Kişi Sayısı Bilgisi</param>
+    private static void RabbitMQKuyrugaGonder(int kisiSayisi)
     {
-        for (int i = 0; i < sayac; i++)
+        for (int i = 0; i < kisiSayisi; i++)
         {
-            var testUsers = new Faker<Employee>()
-                .CustomInstantiator(f => new Employee())
-                    .RuleFor(u => u.Name, f => f.Name.FirstName())
-                    .RuleFor(u => u.Surname, f => f.Name.LastName())
-                    .RuleFor(u => u.BirthPlace, (f, u) => f.Address.City())
-                    .RuleFor(u => u.BirthDate, (f, u) => f.Person.DateOfBirth)
+            var kisiOlusturucu = new Faker<Kisi>()
+                .CustomInstantiator(f => new Kisi())
+                    .RuleFor(u => u.Adi, f => f.Name.FirstName())
+                    .RuleFor(u => u.Soyadi, f => f.Name.LastName())
+                    .RuleFor(u => u.DogumYeri, (f, u) => f.Address.City())
+                    .RuleFor(u => u.DogumTarihi, (f, u) => f.Person.DateOfBirth)
                     .RuleFor(u => u.ID, f => new Random().Next());
 
-            var employee = testUsers.Generate();
+            var kisi = kisiOlusturucu.Generate();
 
+            // Bilgisayara kurulan localhost RabbitMQ Sunucusuna bağlantı için gerekli bilgileri içeren nesnedir
             var factory = new ConnectionFactory() { HostName = "localhost" };
             using (IConnection connection = factory.CreateConnection())
             using (IModel channel = connection.CreateModel())
             {
+                // burada coderserdar adında bir kanal açarak, onun içerisine ilgili kişi kayıtlarının gönderilmesi için hazırlamaktadır
+                // diğer parametreler de kullanılarak kanalın kullanımı özelleştirilebilir
                 channel.QueueDeclare(queue: "coderserdar", durable: false, exclusive: false, autoDelete: false, arguments: null);
                 
-                string message = JsonConvert.SerializeObject(employee);
+                string message = JsonConvert.SerializeObject(kisi);
+                // sınıf serialize edilerek byte dizisi haline dönüştürülür
                 var body = Encoding.UTF8.GetBytes(message);
 
+                // burada dönüştürülen byte dizisinin kanala gönderilmesi sağlanır
                 channel.BasicPublish(exchange: "", routingKey: "coderserdar", basicProperties: null, body: body);
 
-                Console.WriteLine($"Gönderilen kişi: Adı Soyadı: {employee.Name} {employee.Surname} Doğum Tarihi: {employee.BirthDate.ToShortDateString()}");
+                Console.WriteLine($"Gönderilen kişi: Adı Soyadı: {kisi.Adi} {kisi.Soyadi} Doğum Tarihi: {kisi.DogumTarihi.ToShortDateString()}");
                 Console.WriteLine((i + 1) + ". kişi gönderildi...");
             }
         }
     }
 
+    /// <summary>
+    /// Rabbit MQ kuyruğunda biriken bilgilerin FIFO mantığına göre
+    /// Kuyruktan alınarak
+    /// Kişi bilgilerinin konsol ekranında görüntülenmesini sağlayan metottur
+    /// </summary>
     private static void RabbitMQKuyruktanAl()
     {
+        // Bilgisayara kurulan localhost RabbitMQ Sunucusuna bağlantı için gerekli bilgileri içeren nesnedir
         var factory = new ConnectionFactory() { HostName = "localhost" };
         using (IConnection connection = factory.CreateConnection())
         using (IModel channel = connection.CreateModel())
         {
+            // burada coderserdar adında bir kanal açarak, onun içerisine ilgili kişi kayıtlarının gönderilmesi için hazırlamaktadır
+            // diğer parametreler de kullanılarak kanalın kullanımı özelleştirilebilir
             channel.QueueDeclare(queue: "coderserdar", durable: false, exclusive: false, autoDelete: false, arguments: null);
 
+            // burada kuyruğu tüketecek bir comsumer nesnesi oluşturulur ve tüketmeye başlar
             var consumer = new EventingBasicConsumer(channel);
             consumer.Received += (model, ea) =>
             {
                 var body = ea.Body;
                 var message = Encoding.UTF8.GetString(body.ToArray());
-                Employee employee = JsonConvert.DeserializeObject<Employee>(message);
-                Console.WriteLine($"Adı Soyadı: {employee.Name} {employee.Surname} [{employee.BirthPlace}]");
+                Kisi kisi = JsonConvert.DeserializeObject<Kisi>(message);
+                Console.WriteLine($"Adı Soyadı: {kisi.Adi} {kisi.Soyadi} [{kisi.DogumYeri}]");
                 Console.WriteLine("RabbitMQ ile tanıştınız. İyi günler.");
             };
 
+            // kanalın kuyruğu tüketerek ilgili bilgilerin konsolda gösterilmesi sağlanır
             channel.BasicConsume(queue: "coderserdar", autoAck: true, consumer: consumer);
 
             Console.ReadLine();
         }
+        Console.WriteLine("Kuyruğun tamamı tüketildi ve gösterildi. Devam etmek için bir tuşa basınız");
+        Console.ReadLine();
     }
 }
